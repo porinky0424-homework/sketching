e78f71403f919d1e4fda1ba9f6eb9d4fff61f635
Daichi Kato <daichi5967@docomo.ne.jp>
2022-04-24 12:03:51 +0900

    Clear Error by patch-package

---
+++ memo.md
@@ -0,0 +1,3 @@
+## react-draggableにchildrenの型定義がない？問題
+
+https://bagelee.com/programming/javascript-2/patch-package/ のようにpatch-packageをつかうことで解決した。
--- src/components/iconicElement.tsx
+++ src/components/iconicElement.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef} from 'react'
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
+import React, {useEffect} from 'react'
 import { Info } from './field'
 
 type IconicElementShape = 'circle' | 'rectangle' | 'triangle'
@@ -11,11 +11,10 @@ interface Props {
 }
 
 export default function IconicElement({ id, info, iconicElementShape }: Props) {
-  useEffect(() => {
-    console.log(id, info)
-  }, [id, info])
   return (
-    <Draggable>
+    <Draggable
+      position={{x: info.position.x, y: info.position.y}}
+    >
       <div>I can now be moved around!</div>
     </Draggable>
   );
